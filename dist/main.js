c80b04f258061c630f513c8787002290e32eec95
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-25 19:20:06 -0700

    add latest

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ var roleUpgrader = require("roles_upgrader");
 var roleBuilder = require("roles_builder");
 
 module.exports.loop = function() {
-	console.log("am I working?");
+	console.log("I'm working!!");
 	var tower = Game.getObjectById("TOWER_ID");
 	if (tower) {
 		var closestDamagedStructure = tower.pos.findClosestByRange(
@@ -20,6 +20,64 @@ module.exports.loop = function() {
 		if (closestHostile) {
 			tower.attack(closestHostile);
 		}
+	}
+	for (var name in Memory.creeps) {
+		if (!Game.creeps[name]) {
+			delete Memory.creeps[name];
+			console.log("Clearing non-existing creep memory:", name);
+		}
+	}
+
+	var harvesters = _.filter(
+		Game.creeps,
+		creep => creep.memory.role == "harvester"
+	);
+	console.log("Harvesters: " + harvesters.length);
+
+	if (harvesters.length < 3) {
+		var newName = "Harvester" + Game.time;
+		console.log("Spawning new harvester: " + newName);
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
+			memory: { role: "harvester" }
+		});
+	}
+
+	var builders = _.filter(
+		Game.creeps,
+		creep => creep.memory.role == "builder"
+	);
+	console.log("Builders: " + builders.length);
+
+	if (builders.length < 2) {
+		var newName = "Builder" + Game.time;
+		console.log("Spawning new builder: " + newName);
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
+			memory: { role: "builder" }
+		});
+	}
+
+	var upgraders = _.filter(
+		Game.creeps,
+		creep => creep.memory.role == "upgrader"
+	);
+	console.log("Upgraders: " + upgraders.length);
+
+	if (upgraders.length < 2) {
+		var newName = "Upgrader" + Game.time;
+		console.log("Spawning new upgrader: " + newName);
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
+			memory: { role: "upgrader" }
+		});
+	}
+
+	if (Game.spawns["Home"].spawning) {
+		var spawningCreep = Game.creeps[Game.spawns["Home"].spawning.name];
+		Game.spawns["Home"].room.visual.text(
+			"🛠️" + spawningCreep.memory.role,
+			Game.spawns["Home"].pos.x + 1,
+			Game.spawns["Home"].pos.y,
+			{ align: "left", opacity: 0.8 }
+		);
 	}
 
 	for (var name in Game.creeps) {
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var roleUpgrader = require("roles_upgrader");
 var roleBuilder = require("roles_builder");
 
 module.exports.loop = function() {
-	console.log("I'm working");
+	console.log("I'm working!!");
 	var tower = Game.getObjectById("TOWER_ID");
 	if (tower) {
 		var closestDamagedStructure = tower.pos.findClosestByRange(
@@ -34,20 +34,48 @@ module.exports.loop = function() {
 	);
 	console.log("Harvesters: " + harvesters.length);
 
-	if (harvesters.length < 2) {
+	if (harvesters.length < 3) {
 		var newName = "Harvester" + Game.time;
 		console.log("Spawning new harvester: " + newName);
-		Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
 			memory: { role: "harvester" }
 		});
 	}
 
-	if (Game.spawns["Spawn1"].spawning) {
-		var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
-		Game.spawns["Spawn1"].room.visual.text(
+	var builders = _.filter(
+		Game.creeps,
+		creep => creep.memory.role == "builder"
+	);
+	console.log("Builders: " + builders.length);
+
+	if (builders.length < 2) {
+		var newName = "Builder" + Game.time;
+		console.log("Spawning new builder: " + newName);
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
+			memory: { role: "builder" }
+		});
+	}
+
+	var upgraders = _.filter(
+		Game.creeps,
+		creep => creep.memory.role == "upgrader"
+	);
+	console.log("Upgraders: " + upgraders.length);
+
+	if (upgraders.length < 2) {
+		var newName = "Upgrader" + Game.time;
+		console.log("Spawning new upgrader: " + newName);
+		Game.spawns["Home"].spawnCreep([WORK, CARRY, MOVE], newName, {
+			memory: { role: "upgrader" }
+		});
+	}
+
+	if (Game.spawns["Home"].spawning) {
+		var spawningCreep = Game.creeps[Game.spawns["Home"].spawning.name];
+		Game.spawns["Home"].room.visual.text(
 			"🛠️" + spawningCreep.memory.role,
-			Game.spawns["Spawn1"].pos.x + 1,
-			Game.spawns["Spawn1"].pos.y,
+			Game.spawns["Home"].pos.x + 1,
+			Game.spawns["Home"].pos.y,
 			{ align: "left", opacity: 0.8 }
 		);
 	}
