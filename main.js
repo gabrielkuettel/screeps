eb261fc18cbf3417917cdcef90396b5ace4f1865
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-25 17:51:06 -0700

    fix imports

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var roleHarvester = require("roles/role.harvester");
-var roleUpgrader = require("roles/role.upgrader");
-var roleBuilder = require("roles/role.builder");
+var roleHarvester = require("./roles/role.harvester");
+var roleUpgrader = require("./roles/role.upgrader");
+var roleBuilder = require("./roles/role.builder");
 
 module.exports.loop = function() {
 	console.log("am I working?");
