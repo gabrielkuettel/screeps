91454ab9d4915d2a890dbf9828ad656fa5157a65
Gabriel Kuettel <gabrielkuettel@gmail.com>
2019-11-30 01:14:45 -0700

    dist

---
+++ dist/roles_Gravedigger.js
@@ -0,0 +1,58 @@
+const Base = require("roles_Base");
+
+// inherits
+// this.creep, this.state
+// this.recycle(), this.logger(), this.setState()
+
+class Gravedigger extends Base {
+	constructor(creep, newState = null, timeToLive = 100) {
+		super(creep);
+		this.newState = newState;
+		this.timeToLive = timeToLive;
+	}
+
+	run() {
+		this.controller();
+		this.setState(this.newState);
+	}
+
+	controller() {
+		const { terminate, log, talk, digging, freeCapacity } = this.state;
+
+		if (log) {
+			this.logger(["log", "terminate", "freeCapacity", "digging", "action"]);
+		}
+
+		if (talk) {
+			this.talk();
+		}
+
+		if (this.creep.ticksToLive < this.timeToLive) {
+			this.setState({ terminate: true });
+		}
+
+		if (terminate === true) {
+			return this.recycle();
+		}
+
+		if (freeCapacity > 0) {
+			this.setState({ digging: true });
+		}
+
+		if (freeCapacity === 0) {
+			this.setState({ digging: false });
+		}
+
+		if (digging) {
+			console.log("digging now");
+			return this.withdrawFromSource(FIND_TOMBSTONES, "⚰️");
+		}
+
+		if (!digging) {
+			console.log("deposit now");
+			return this.deposit(0);
+		}
+	}
+}
+
+module.exports = Gravedigger;
---
+++ dist/roles_Harvester.js
@@ -0,0 +1,61 @@
+const Base = require("roles_Base");
+
+// inherits
+// this.creep, this.state
+// this.recycle(), this.logger(), this.setState()
+
+class Harvester extends Base {
+	constructor(creep, newState = null, timeToLive = 100) {
+		super(creep);
+		this.newState = newState;
+		this.timeToLive = timeToLive;
+	}
+
+	run() {
+		this.controller();
+		this.setState(this.newState);
+	}
+
+	controller() {
+		const { terminate, log, talk, harvesting, freeCapacity } = this.state;
+
+		if (log) {
+			this.logger([
+				"log",
+				"terminate",
+				"freeCapacity",
+				"harvesting",
+				"action"
+			]);
+		}
+
+		if (talk) {
+			this.talk();
+		}
+
+		if (this.creep.ticksToLive < this.timeToLive) {
+			this.setState({ terminate: true });
+		}
+
+		if (terminate === true) {
+			return this.recycle();
+		}
+
+		if (freeCapacity > 0) {
+			this.setState({ harvesting: true });
+		} else {
+			this.setState({ harvesting: false });
+		}
+
+		if (harvesting) {
+			return this.harvest(1);
+		}
+
+		if (!harvesting) {
+			return this.drop();
+			// return this.deposit();
+		}
+	}
+}
+
+module.exports = Harvester;
